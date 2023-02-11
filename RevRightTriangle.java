e9b3ca66f7c9402db1c37d50e0006e32b3361c4f
Aditya Gupta <117362157+adityaxcix@users.noreply.github.com>
2023-02-11 19:44:40 +0530

    Add files via upload

---
+++ RevRightTriangle.java
@@ -0,0 +1,20 @@
+public class RevRightTriangle 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		for(int i =1;i<=5;i++)
+		{
+			for(int j =i;j<=5;j++ )
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
